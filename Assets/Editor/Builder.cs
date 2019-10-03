482626cdd96b7a04534c65286b45a832b187b9cc
o92design <o92design@gmail.com>
2019-10-03 23:15:27 +0200

    [#1] Create Unity Project * Created a basic build system

---
+++ Assets/Editor/Builder.cs
@@ -0,0 +1,11 @@
+using System;
+using editor;
+
+public class Builder
+{
+  public static void CommandLineBuild()
+  {
+    Console.WriteLine("Performing a Command Line Build");
+    Build.CommandLineBuild();
+  }
+}
---
+++ Assets/Editor/Tests/Test_BasicUnityExamples.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using NUnit.Framework;
+using UnityEngine;
+using UnityEngine.TestTools;
+
+namespace Tests
+{
+  public class Test_BasicUnityExamples
+  {
+    // A Test behaves as an ordinary method
+    [Test]
+    public void BasicDivisionTest()
+    {
+      int x = 10;
+      int y = 5;
+      int correctAnswer = 2;
+
+      Assert.AreEqual(correctAnswer, x / y);
+    }
+  }
+}
