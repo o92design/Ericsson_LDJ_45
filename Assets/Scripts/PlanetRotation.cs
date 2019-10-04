c51cdc883af97116bcb67c9b5cd56e744baf42e5
Andreas Fransson <Ankanx@gmail.com>
2019-10-05 01:21:04 +0200

    Concept of rotating planet attaching garbage, idea is to combo iteams into spawning wildlife etc and try not to implode too fast.
    Known Issues:
    Fixedjointes created on each collition, and this script is not disabled after the first creation meaning multiple joints are currently created for each cube.

---
+++ Assets/Scripts/PlanetRotation.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlanetRotation : MonoBehaviour
+{
+    public float m_rotationalSpeed = 25f;
+    public float m_accelerationMultiplier = 10f;
+    // Start is called before the first frame update
+    void Start()
+    {
+        InvokeRepeating("Acceleration",7f, 7f);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        transform.Rotate(new Vector3(Random.value, Random.value, Random.value) * (m_rotationalSpeed *  Time.deltaTime));
+
+        transform.RotateAround(Vector3.zero, Vector3.forward, 1.0f);
+
+    }
+
+
+    public void Acceleration()
+    {
+        m_rotationalSpeed += m_accelerationMultiplier;
+    }
+}
