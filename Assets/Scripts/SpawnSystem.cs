78327f22128fa778aa0045d4cc066a11b4beb01e
Andreas Fransson <Ankanx@gmail.com>
2019-10-05 15:24:19 +0200

    Added Strössel functionality.

--- Assets/Scripts/SpawnSystem.cs
+++ Assets/Scripts/SpawnSystem.cs
@@ -7,6 +7,7 @@ public class SpawnSystem : MonoBehaviour
 
     public GameObject m_SpawnPoint;
     public GameObject m_debugSpawnObject;
+    public int m_maxNumberOfStrössel = 20;
     // Start is called before the first frame update
     void Start()
     {
@@ -22,6 +23,14 @@ public class SpawnSystem : MonoBehaviour
     public void DebugSpawn()
     {
         SpawnObject(m_debugSpawnObject);
+    }
+
+    public void Strössla(GameObject strössel)
+    {
+        for (int i = 0; i < Random.Range(2, m_maxNumberOfStrössel); i++)
+        {
+            SpawnObject(strössel);
+        }
     }
 
     public void SpawnObject(GameObject objectToSPawn)
