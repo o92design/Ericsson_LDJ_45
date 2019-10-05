d1682ab52656593f179fc2f5aa3f8c348f1368e9
Andreas Fransson <Ankanx@gmail.com>
2019-10-05 18:01:32 +0200

    Added audiomanager and test sound.

---
+++ Assets/AudioManager.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AudioManager : MonoBehaviour
+{
+
+    // Audio Clips Here
+    //AudioClip m_sound; <--- example
+    public AudioClip m_blip;
+
+
+    // Components
+    AudioListener m_audioListener;
+    AudioSource m_audioSource;
+    // Start is called before the first frame update
+    void Start()
+    {
+        //m_sound = Resources.Load<AudioClip>("Sound/m_sound"); <-- example
+        m_blip = Resources.Load<AudioClip>("Sound/Blip");
+        m_audioListener = GetComponent<AudioListener>();
+        m_audioSource = GetComponent<AudioSource>();
+
+
+
+
+        // Init
+        m_audioSource.PlayOneShot(m_blip);
+        
+    }
+
+
+    public void Keyevent()
+    {
+        m_audioSource.PlayOneShot(m_blip);
+    }
+
+
+
+}
