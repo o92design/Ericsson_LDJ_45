34a179d4f4abc117dd46169b217ef6e479d89a64
Oskar Månsson <o92design@gmail.com>
2019-10-05 18:48:54 +0200

    [#9] Scene switcher
    Nu kan man byta scene med left Ctrl+Left shift + scene index i Build settings OBS 9 == 0 då jag inte fick 0 att funka

--- Assets/Scripts/GameManager.cs
+++ Assets/Scripts/GameManager.cs
@@ -1,7 +1,8 @@
-﻿using System.Collections;
-using System.Collections.Generic;
+﻿using System.Collections.Generic;
+using UnityEditor;
 using UnityEngine;
 using UnityEngine.SceneManagement;
+using System.IO;
 
 public class GameManager : MonoBehaviour
 {
@@ -19,31 +20,57 @@ public class GameManager : MonoBehaviour
     //If instance already exists and it's not this:
     else if (instance != this)
 
-    //Then destroy this. This enforces our singleton pattern, meaning there can only ever be one instance of a GameManager.
-    Destroy(gameObject);
+      //Then destroy this. This enforces our singleton pattern, meaning there can only ever be one instance of a GameManager.
+      Destroy(gameObject);
 
     //Sets this to not be destroyed when reloading scene
     DontDestroyOnLoad(gameObject);
   }
 
-
-  void Update()
+  void OnGUI()
   {
-    if(Input.GetKey(KeyCode.LeftControl) && 
-       Input.GetKey(KeyCode.LeftShift))
+    if (Input.GetKey(KeyCode.LeftControl) &&
+        Input.GetKey(KeyCode.LeftShift))
     {
-      Debug.Log("Tries to switch scene");
-     
       Event keyEvent = Event.current;
-      if(keyEvent != null)
+
+      if (keyEvent.isKey && keyEvent.type == EventType.KeyDown)
       {
-        Debug.Log(keyEvent.keyCode);
         if (keyEvent.keyCode >= KeyCode.Alpha0 && keyEvent.keyCode <= KeyCode.Alpha9)
         {
           int sceneIndex = keyEvent.keyCode - KeyCode.Alpha0;
-          SceneManager.LoadScene(sceneIndex);
+          // 0 wasn't recorded as 0 :(
+          if (keyEvent.keyCode == KeyCode.Alpha9)
+            sceneIndex = 0;
+
+          if (sceneIndex < SceneManager.sceneCountInBuildSettings)
+            SceneManager.LoadScene(sceneIndex);
         }
       }
     }
+  }
+
+  public List<string> GetAllScenes()
+  {
+    List<string> projectScenes = new List<string>();
+    var scenesDirPath = Application.dataPath + "/Scenes";
+    var scenesDirInfo = new DirectoryInfo(scenesDirPath);
+    var allScenesInfo = scenesDirInfo.GetFiles("*.unity", SearchOption.AllDirectories);
+
+    foreach (var sceneInfo in allScenesInfo)
+    {
+      projectScenes.Add(sceneInfo.FullName);
+    }
+
+    Debug.Log("Chose to add all scenes: " + projectScenes.Count);
+
+    string scenesAdded = "";
+    foreach (string sceneName in projectScenes)
+    {
+      scenesAdded += sceneName + "\n";
+    }
+    Debug.Log(scenesAdded);
+
+    return projectScenes;
   }
 }
