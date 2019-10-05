ce86ec78f780ad17d9f78b03a2dee20f45061668
Daniel Arnqvist <daniel.arnqvist@gmail.com>
2019-10-05 23:24:37 +0200

    #20 Add input manager controls for planet

--- Assets/Scripts/PlanetRotation.cs
+++ Assets/Scripts/PlanetRotation.cs
@@ -6,24 +6,43 @@ public class PlanetRotation : MonoBehaviour
 {
     public float m_rotationalSpeed = 10f;
     public float m_accelerationMultiplier = 5f;
+    Vector3 m_rotation = new Vector3();
     // Start is called before the first frame update
     void Start()
-    {
-        InvokeRepeating("Acceleration",5f, 5f);
-    }
+        {
+            InvokeRepeating("Acceleration",5f, 5f);
+        }
 
     // Update is called once per frame
     void Update()
-    {
-        transform.Rotate(new Vector3(Random.value, Random.value, Random.value) * (m_rotationalSpeed *  Time.deltaTime));
+        {
+            m_rotation += new Vector3(Random.value/10, Random.value/10, Random.value/10);
 
-        transform.RotateAround(Vector3.zero, Vector3.forward, 1.0f);
 
-    }
+            if(Input.GetKey(InputManager.Instance.right))
+            {
+                m_rotation += new Vector3(0.1f, 0, 0);
+            }
+            if(Input.GetKey(InputManager.Instance.left))
+            {
+                m_rotation += new Vector3(-0.1f, 0, 0);
+            }
+            if(Input.GetKey(InputManager.Instance.up))
+            {
+                m_rotation += new Vector3(0, 0.1f, 0);
+            }
+            if(Input.GetKey(InputManager.Instance.down))
+            {
+                m_rotation += new Vector3(0, -0.1f, 0);
+            }
+
+            m_rotation.Normalize();
+            transform.Rotate(m_rotation * (m_rotationalSpeed * Time.deltaTime), Space.Self);
+        }
 
 
     public void Acceleration()
-    {
-        m_rotationalSpeed += m_accelerationMultiplier;
-    }
+        {
+            m_rotationalSpeed += m_accelerationMultiplier;
+        }
 }
