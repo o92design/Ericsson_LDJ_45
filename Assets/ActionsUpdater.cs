6a44bfb5f60ffd1773e6f73d8fb55ff962cf5fe9
Oskar Månsson <o92design@gmail.com>
2019-10-06 23:06:29 +0200

    Game steps

---
+++ Assets/ActionsUpdater.cs
@@ -0,0 +1,45 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+
+public class ActionsUpdater : MonoBehaviour
+{
+  public Text m_ActionText;
+  public Text m_EventText;
+  public GameSteps m_gameSteps;
+  public int m_flowStep = 0;
+
+  public void UpdateAction(KeyCode p_key)
+  {
+    PlayerActions action = m_gameSteps.m_eventActions[++m_flowStep];
+    string actionText = action.m_action;
+
+    if (m_flowStep != 0)
+      actionText += " of " + p_key.ToString() + ",";
+    else
+      actionText += ",";
+
+    m_ActionText.text = actionText;
+    m_gameSteps.UpdateAction(m_flowStep, p_key, true);
+  }
+
+  public void UpdateEventText()
+  {
+    m_EventText.text = "then " + m_gameSteps.m_eventActions[m_flowStep + 1].m_action.ToLower();
+  }
+
+  public void OnGUI()
+  {
+    Event keyEvent = Event.current;
+
+    if(keyEvent.isKey)
+    {
+      if(m_flowStep == 0 ||
+        (keyEvent.keyCode != m_gameSteps.m_eventActions[m_flowStep].m_key && Input.GetKeyDown(keyEvent.keyCode)))
+      {
+        UpdateAction(keyEvent.keyCode);
+        UpdateEventText();
+      }
+    }
+  }
+
+}
---
+++ Assets/GameSteps.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[Serializable]
+public struct PlayerActions
+{
+  public string m_action;
+  public KeyCode m_key;
+  public bool m_available;
+}
+
+public class GameSteps : MonoBehaviour
+{
+  public PlayerActions[] m_eventActions;
+
+  public void UpdateAction(int p_index, KeyCode p_key, bool p_available)
+  {
+    m_eventActions[p_index].m_key = p_key;
+    m_eventActions[p_index].m_available = p_available;
+  }
+}
+
