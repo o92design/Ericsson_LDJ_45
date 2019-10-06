962c8137387189ed62b709a55fdab48ed1322e20
Andreas Fransson <Ankanx@gmail.com>
2019-10-06 16:35:53 +0200

    Added ingame Menu with exit functionality and pause.

---
+++ Assets/IngameUiHandler.cs
@@ -0,0 +1,46 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class IngameUiHandler : MonoBehaviour
+{
+
+
+
+    public static bool m_paused = false;
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Escape))
+        {
+            GetComponent<Canvas>().enabled = !GetComponent<Canvas>().enabled;
+            m_paused = !m_paused;
+            if (m_paused)
+            {
+                Time.timeScale = 0f;
+            }
+            else
+            {
+                Time.timeScale = 1f;
+            }
+
+        }
+    }
+
+
+
+
+    public void ExitApplication()
+    {
+        Application.Quit();
+    }
+
+}
