1d17e545f705d8ad2da5fcd767661dc865724dcd
Oskar Månsson <o92design@gmail.com>
2019-10-07 18:40:55 +0200

    Action update now has a Input delay
    We can't update other action right after oneanother

--- Assets/ActionsUpdater.cs
+++ Assets/ActionsUpdater.cs
@@ -1,4 +1,5 @@
-﻿using UnityEngine;
+﻿using System.Collections;
+using UnityEngine;
 using UnityEngine.Playables;
 using UnityEngine.UI;
 
@@ -8,7 +9,10 @@ public class ActionsUpdater : MonoBehaviour
   public Text m_EventText;
   public GameSteps m_gameSteps;
   public int m_flowStep = 0;
- 
+  [Range(0, 30)]
+  public float m_inputWaitingTime = 5;
+  public bool m_canInput;
+
   public void UpdateAction(KeyCode p_key)
   {
     // check if we're on exploration step now
@@ -35,11 +39,11 @@ public class ActionsUpdater : MonoBehaviour
 
   public void UpdateEventText()
   {
-    if(m_flowStep + 1 < m_gameSteps.m_playerActions.Length)
+    if (m_flowStep + 1 < m_gameSteps.m_playerActions.Length)
     {
       m_EventText.text = "then " + m_gameSteps.m_playerActions[m_flowStep + 1].m_action.ToLower();
     }
-    else if(m_flowStep + 1 == m_gameSteps.m_playerActions.Length)
+    else if (m_flowStep + 1 == m_gameSteps.m_playerActions.Length)
     {
       m_EventText.text = "then god must explore";
       Debug.Log("We're on Exploration step");
@@ -52,15 +56,26 @@ public class ActionsUpdater : MonoBehaviour
   {
     Event keyEvent = Event.current;
 
-    if(keyEvent.isKey)
+    if (keyEvent.isKey && m_canInput)
     {
       // Update next step
-      if(m_flowStep == 0 ||
+      if (m_flowStep == 0 ||
         (!m_gameSteps.IsKeyMapped(keyEvent.keyCode) && Input.GetKeyDown(keyEvent.keyCode)))
       {
-        UpdateAction(keyEvent.keyCode);
-        UpdateEventText();
+        StartCoroutine(WaitForSomeTime(keyEvent.keyCode, m_inputWaitingTime));
       }
     }
+  }
+
+  IEnumerator WaitForSomeTime(KeyCode p_keyCode, float p_seconds)
+  {
+    m_canInput = false;
+    UpdateAction(p_keyCode);
+    UpdateEventText();
+
+    yield return new WaitForSeconds(p_seconds);
+
+    Debug.Log("Now we can input again!");
+    m_canInput = true;
   }
 }
