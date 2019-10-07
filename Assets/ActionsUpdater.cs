e9613494dfae4bc08e8862affed7385afe5ccfca
Oskar Månsson <o92design@gmail.com>
2019-10-07 20:07:40 +0200

    Timed sequences with animations
    Updated some UI elements

--- Assets/ActionsUpdater.cs
+++ Assets/ActionsUpdater.cs
@@ -8,7 +8,17 @@ public class ActionsUpdater : MonoBehaviour
   public Text m_EventText;
   public GameSteps m_gameSteps;
   public int m_flowStep = 0;
- 
+  [Range(0, 30)]
+  public float m_inputWaitingTime = 5;
+  public bool m_canInput;
+  public float m_InputTimeAfterAfterFadeInAnimation = 5;
+  public float m_bigBangTextDelay;
+
+  public void Start()
+  {
+    StartCoroutine(FadeInStartingText(m_InputTimeAfterAfterFadeInAnimation));
+  }
+
   public void UpdateAction(KeyCode p_key)
   {
     // check if we're on exploration step now
@@ -17,11 +27,6 @@ public class ActionsUpdater : MonoBehaviour
 
     PlayerActions action = m_gameSteps.m_playerActions[++m_flowStep];
     string actionText = action.m_action;
-
-    if (action.m_action.ToLower() == "sound")
-    {
-      GetComponent<PlayableDirector>().Play();
-    }
 
     if (m_flowStep != 0)
       actionText += " of " + p_key.ToString() + ",";
@@ -48,6 +53,8 @@ public class ActionsUpdater : MonoBehaviour
     }
   }
 
+
+  // This handles our Step updates
   public void OnGUI()
   {
     Event keyEvent = Event.current;
@@ -59,9 +66,52 @@ public class ActionsUpdater : MonoBehaviour
       if(m_flowStep == 0 ||
         (!m_gameSteps.IsKeyMapped(keyEvent.keyCode) && Input.GetKeyDown(keyEvent.keyCode)))
       {
-        UpdateAction(keyEvent.keyCode);
-        UpdateEventText();
+        if (m_flowStep == 0)
+        {
+          GetComponent<PlayableDirector>().Play();
+        }
+
+        StartCoroutine(UpdateText(keyEvent.keyCode, m_inputWaitingTime));
       }
     }
+  }
+
+  IEnumerator UpdateText(KeyCode p_keyCode, float p_seconds)
+  {
+    m_ActionText.gameObject.GetComponent<Animation>().Play("TextAnimation_FadeOut");
+    m_EventText.gameObject.GetComponent<Animation>().Play("TextAnimation_FadeOut");
+    m_canInput = false;
+
+    float secondsMultiplyer = 0;
+
+    yield return new WaitForSeconds(p_seconds);
+    UpdateAction(p_keyCode);
+    UpdateEventText();
+
+    if (m_flowStep == 2)
+    {
+      secondsMultiplyer += m_bigBangTextDelay;
+    }
+
+    yield return new WaitForSeconds(p_seconds + secondsMultiplyer);
+    m_ActionText.gameObject.GetComponent<Animation>().Play("TextAnimation_FadeIn");
+    m_EventText.gameObject.GetComponent<Animation>().Play("TextAnimation_FadeIn");
+
+    m_gameSteps.m_playerActions[m_flowStep].m_AvailText.gameObject.GetComponent<Animation>().Play();
+
+    yield return new WaitForSeconds(2);
+    Debug.Log("Now we can input again!");
+
+    m_canInput = true;
+  }
+
+  IEnumerator FadeInStartingText(float p_seconds)
+  {
+    m_canInput = false;
+    m_ActionText.gameObject.GetComponent<Animation>().Play("TextAnimation_FadeIn");
+    m_EventText.gameObject.GetComponent<Animation>().Play("TextAnimation_FadeIn");
+
+    yield return new WaitForSeconds(p_seconds);
+    m_canInput = true;
   }
 }
