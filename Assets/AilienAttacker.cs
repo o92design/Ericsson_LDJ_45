2161d884d17a765f3bf14cc5684f38534dbbd098
Andreas Fransson <Ankanx@gmail.com>
2019-10-07 20:40:05 +0200

    Added exploding planet

--- Assets/AilienAttacker.cs
+++ Assets/AilienAttacker.cs
@@ -20,10 +20,13 @@ public class AilienAttacker : MonoBehaviour
     float radiusSpeed = 5f;
     float m_firerate = 3f;
     public bool m_shooting = false;
+    public GameObject m_boulder;
+    public bool m_exploded = false;
     // Start is called before the first frame update
     void Start()
     {
         lineRenderer = GetComponent<LineRenderer>();
+        m_boulder = Resources.Load< GameObject > ("Prefabs/rock_001");
     }
 
     // Update is called once per frame
@@ -65,23 +68,30 @@ public class AilienAttacker : MonoBehaviour
             transform.Rotate(Vector3.up * (10 * Time.deltaTime));
             transform.Rotate(Vector3.right * (10 * Time.deltaTime));
             transform.RotateAround(m_starttarget.transform.position, Vector3.up, speed * Time.deltaTime);
-            desiredPosition = (transform.position - m_starttarget.transform.position).normalized * radius + m_starttarget.transform.position ;
+            desiredPosition = (transform.position - new Vector3(m_starttarget.transform.position.x, m_starttarget.transform.position.y, m_starttarget.transform.position.z)).normalized * radius + new Vector3 (m_starttarget.transform.position.x, m_starttarget.transform.position.y, m_starttarget.transform.position.z) ;
             transform.position = Vector3.MoveTowards(transform.position, desiredPosition, radiusSpeed * Time.deltaTime);
 
 
             if (m_firetarget == null && !m_shooting)
             {
-                float distance = 0;
-                foreach (GameObject game in PlanetJointHandler.Instance.m_connected)
+                if (PlanetJointHandler.Instance.m_connected.Count > 0)
                 {
-                    if(game != null)
+                    float distance = 0;
+                    foreach (GameObject game in PlanetJointHandler.Instance.m_connected)
                     {
-                        if (Vector3.Distance(this.transform.position, game.transform.position) < distance || distance == 0)
+                        if (game != null)
                         {
-                            distance = Vector3.Distance(this.transform.position, game.transform.position);
-                            m_firetarget = game;
+                            if (Vector3.Distance(this.transform.position, game.transform.position) < distance || distance == 0)
+                            {
+                                distance = Vector3.Distance(this.transform.position, game.transform.position);
+                                m_firetarget = game;
+                            }
                         }
                     }
+                }
+                else
+                {
+                    m_firetarget = PlanetJointHandler.Instance.gameObject;
                 }
             }
 
@@ -109,14 +119,42 @@ public class AilienAttacker : MonoBehaviour
 
     IEnumerator ResetLazer(LineRenderer lazer)
     {
-        AudioManager.Instance.PlayLaser();
-        yield return new WaitForSeconds(0.1f);
-        m_shooting = true;
-        lineRenderer.enabled = true;
-        yield return new WaitForSeconds(0.5f);
-        lazer.enabled = false;
-        PlanetJointHandler.Instance.m_connected.Remove(m_firetarget);
-        Destroy(m_firetarget);
+        if (!m_exploded)
+        {
+            AudioManager.Instance.PlayLaser();
+            yield return new WaitForSeconds(0.1f);
+            m_shooting = true;
+            lineRenderer.enabled = true;
+            yield return new WaitForSeconds(0.5f);
+            lazer.enabled = false;
+        }
+        if (m_firetarget == PlanetJointHandler.Instance.gameObject)
+        {
+            m_firetarget.transform.localScale = 0.8f * m_firetarget.transform.localScale;
+            if (m_firetarget.transform.localScale.x < 1.5f && !m_exploded)
+            {
+                for (int i = 0; i < 50; i++)
+                {
+
+                    GameObject obj = Instantiate(m_boulder, m_firetarget.transform.position,Quaternion.identity);
+                    obj.GetComponent<MeshCollider>().enabled = false;
+                    obj.GetComponent<Rigidbody>().useGravity = false;
+                    obj.transform.localScale = Random.Range(1, 7) * obj.transform.localScale;
+                    transform.rotation = Random.rotation;
+                    obj.GetComponent<Rigidbody>().velocity = -Random.rotation.eulerAngles / 15;
+                    obj.GetComponent<Rigidbody>().AddTorque(obj.transform.up * 10);
+                }
+                // destroy bam
+                m_firetarget.transform.localScale = Vector3.zero;
+                AudioManager.Instance.PlayBoom();
+                m_exploded = true;
+            }
+        }
+        else
+        {
+            PlanetJointHandler.Instance.m_connected.Remove(m_firetarget);
+            Destroy(m_firetarget);
+        }
         m_shooting = false;
     }
 
