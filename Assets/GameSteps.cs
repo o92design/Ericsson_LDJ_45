88f3a4c7fdac35e071fa8bc02b0847349a75981a
Oskar Månsson <o92design@gmail.com>
2019-10-07 13:29:23 +0200

    Game flow now work properly
    Jag dribblade bort mig själv när jag gjorde min egen Andreas_dev_scene till Oskar_game_scene - den senare är nu borttagen

---
+++ Assets/GameSteps.cs
@@ -0,0 +1,94 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.Playables;
+
+[Serializable]
+public struct PlayerActions
+{
+  public string m_action;
+  public KeyCode m_key;
+  public bool m_available;
+  private SpawnSystem SpawnSystem;
+  public Text m_AvailText;
+
+  public GameObject m_objectToSpawn;
+  public GameObject m_spawnPosition;
+
+
+  public void initializeAction()
+  {
+    SpawnSystem = MonoBehaviour.FindObjectOfType<SpawnSystem>();
+  }
+
+  public void Spawn()
+  {
+    if (!m_available)
+      return;
+
+    if (m_objectToSpawn == null)
+      return;
+
+    if (m_action.ToLower() == "light")
+    {
+      m_objectToSpawn.GetComponent<InitiateWorld>().BigBang();
+    }
+    else
+    {
+      SpawnSystem.SpawnObject(m_objectToSpawn, m_spawnPosition.transform.position);
+    }
+  }
+}
+
+public class GameSteps : MonoBehaviour
+{
+  public PlayerActions[] m_playerActions;
+
+  public void UpdateAction(int p_index, KeyCode p_key, bool p_available)
+  {
+    m_playerActions[p_index].m_key = p_key;
+    m_playerActions[p_index].m_available = p_available;
+
+    if (m_playerActions[p_index].m_available)
+    {
+      m_playerActions[p_index].m_AvailText.text = m_playerActions[p_index].m_action + " of " + m_playerActions[p_index].m_key;
+      m_playerActions[p_index].m_AvailText.enabled = p_available;
+      m_playerActions[p_index].Spawn();
+    }
+  }
+
+  public bool IsKeyMapped(KeyCode p_key)
+  {
+    foreach (PlayerActions action in m_playerActions)
+    {
+      if (action.m_key == p_key)
+        return true;
+    }
+    return false;
+  }
+
+  public void OnGUI()
+  {
+    Event keyEvent = Event.current;
+
+    if (keyEvent.isKey)
+    {
+      KeyCode keyInput = keyEvent.keyCode;
+      if (IsKeyMapped(keyInput))
+      {
+        for (int actionIndex = 0; actionIndex < m_playerActions.Length; ++actionIndex)
+        {
+          PlayerActions action = m_playerActions[actionIndex];
+          if (action.m_key == keyInput)
+          {
+            m_playerActions[actionIndex].Spawn();
+            return;
+          }
+        }
+      }
+    }
+  }
+}
+
--- Assets/Scripts/SwitchToGame.cs
+++ Assets/Scripts/SwitchToGame.cs
@@ -6,6 +6,6 @@ public class SwitchToGame : MonoBehaviour
   public string m_nextSceneName;
   private void OnEnable()
   {
-    SceneManager.LoadScene(m_nextSceneName, UnityEngine.SceneManagement.LoadSceneMode.Single);
+    SceneManager.LoadScene(m_nextSceneName);
   }
 }
